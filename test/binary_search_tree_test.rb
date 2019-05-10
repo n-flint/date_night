ba436ec33feea83cf9df1f2aab5282abb5d9fddd
Noah Flint <noah.flint24@gmail.com>
2019-05-10 11:13:57 -0600

    added come extra tests I think I would need

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -4,6 +4,10 @@ class BinarySearchTreeTest < Minitest::Test
 
     def setup
       @tree = BinarySearchTree.new
+      @tree.insert(61, "Bill & Ted's Excellent Adventure")
+      @tree.insert(16, "Johnny English")
+      @tree.insert(92, "Sharknado 3")
+      @tree.insert(50, "Hannibal Buress: Animal Furnace")
     end
 
     def test_it_exists
@@ -11,14 +15,26 @@ class BinarySearchTreeTest < Minitest::Test
     end
 
     def test_it_can_add_nodes
-      @tree.insert(61, "Bill & Ted's Excellent Adventure")
-      @tree.insert(16, "Johnny English")
-      @tree.insert(92, "Sharknado 3")
-      @tree.insert(50, "Hannibal Buress: Animal Furnace")
-      # require 'pry'; binding.pry
+      tree.insert(61, "Bill & Ted's Excellent Adventure")
 
-      assert @tree.include?(61), true
-      assert @tree.include?(72), false
+      require 'pry'; binding.pry
+
+      assert tree.include?(61), true
+      assert tree.include?(72), false
     end
+
+    def test_it_can_get_depth_of_node
+      assert @tree.depth_of(92), 1
+      assert @tree.depth_of(50), 1
+    end
+
+    def test_it_can_get_max_score
+      assert @tree.max, {"Sharknado 3"=>92}
+    end
+
+    def test_it_can_get_min_score
+      assert @tree.min, {"Johnny English"=>16}
+    end
+
 
 end
