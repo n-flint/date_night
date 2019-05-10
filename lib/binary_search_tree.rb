1a8f319f7b899e8bf6292cfc6bd9123fa089c6d4
Noah Flint <noah.flint24@gmail.com>
2019-05-10 11:14:19 -0600

    added some unused methods

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,18 +1,32 @@
 class BinarySearchTree
-  attr_reader :left, :right
+  attr_reader :left, :right, :data
 
   def initialize
     @left = nil
     @right = nil
+    @data = []
   end
 
   def insert(score, title)
     node = Node.new(score, title)
+    @data << node
+    @data
     # require 'pry'; binding.pry
   end
 
   def include?(score)
     # require 'pry'; binding.pry
+  end
+
+  def depth_of(score)
+
+
+  end
+
+  def max
+  end
+
+  def min
   end
 
   
